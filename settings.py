ffe6c5f0ee54bbefda4ca4973a1b42888332da1a
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-08-03 15:14:22 -0600

    Quandl as main doc

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
 import os
 from dotenv import load_dotenv
-from pyzillow.pyzillow import ZillowWrapper
 
 load_dotenv()
-ZILLOW_API_KEY = os.getenv('ZILLOW_API_KEY')
-zillow_data = ZillowWrapper(ZILLOW_API_KEY)
-

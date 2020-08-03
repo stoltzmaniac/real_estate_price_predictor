1398f071630e3bd0280dc0d7b1078b09cdb610a0
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-08-03 14:59:50 -0600

    Basic data ready

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pyzillow.pyzillow import ZillowWrapper
 
-
 load_dotenv()
-
 ZILLOW_API_KEY = os.getenv('ZILLOW_API_KEY')
+zillow_data = ZillowWrapper(ZILLOW_API_KEY)
+
